c01d3c13ddc8b99627fb58736cc52573e9a5341d
Tom O'Connell <toconnel@caltech.edu>
2017-03-07 20:00:12 -0800

    Renamed Olfactometry directory to arduino.

---
+++ CleanValves/CleanValves.ino
@@ -0,0 +1,23 @@
+
+const int min_pin = 5;
+const int max_pin = 11;
+
+void setup() {
+  for (int i=min_pin;i<max_pin;i++) {
+    pinMode(i, OUTPUT);
+    digitalWrite(i, LOW);
+  }
+}
+
+void loop() {
+  for (int i=min_pin;i<max_pin;i++) {
+    pinMode(i, OUTPUT);
+    digitalWrite(i, HIGH);
+  }
+  delay(20000);
+  for (int i=min_pin;i<max_pin;i++) {
+    pinMode(i, OUTPUT);
+    digitalWrite(i, LOW);
+  }
+  delay(1000);
+}
---
+++ TestValves/TestValves.ino
@@ -0,0 +1,34 @@
+
+#define LED 13
+
+const int num_odors = 8;
+const int min_olfactometer_pin = 4;
+const int max_olfactometer_pin = min_olfactometer_pin + num_odors - 1;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.print('Testing olfactometer valves on Arduino pins ');
+  Serial.print(min_olfactometer_pin);
+  Serial.print(' through ');
+  Serial.println(max_olfactometer_pin);
+  
+  for (int i=min_olfactometer_pin;i<=max_olfactometer_pin;i++) {
+    pinMode(i, OUTPUT);
+  }
+  
+  for (int i=min_olfactometer_pin;i<=max_olfactometer_pin;i++) {
+    digitalWrite(i, LOW);
+  }
+}
+
+void loop() {
+  for (int i=min_olfactometer_pin;i<=max_olfactometer_pin;i++) {
+    Serial.println(i);
+    digitalWrite(i, HIGH);
+    digitalWrite(LED, HIGH);
+    delay(1000);
+    digitalWrite(i, LOW);
+    digitalWrite(LED, LOW);
+    delay(1000);
+  }
+}
