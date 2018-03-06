9070f88da5b555673040cf17cf9022913cf773fa
Tom O'Connell <toconnel@caltech.edu>
2018-03-05 16:49:59 -0800

    Reformatting and shorter time-per-valve in test_valves.py

--- nodes/test_valves.py
+++ nodes/test_valves.py
@@ -9,14 +9,17 @@ class ValveTester:
     def __init__(self):
         rospy.init_node('valve_tester')
 
-        self.pin2name = dict(zip(range(54, 70), ['A' + str(i) for i in range(16)]))
+        self.pin2name = dict(zip(range(54, 70), \
+            ['A' + str(i) for i in range(16)]))
         rospy.loginfo('valve_tester waiting for services')
         defaults_service_name = 'load_defaults'
         sequence_service_name = 'load_seq'
         rospy.wait_for_service(defaults_service_name)
         rospy.wait_for_service(sequence_service_name)
-        load_defaults = rospy.ServiceProxy(defaults_service_name, LoadDefaultStates)
-        load_next_sequence = rospy.ServiceProxy(sequence_service_name, LoadSequence)
+        load_defaults = rospy.ServiceProxy(defaults_service_name, \
+            LoadDefaultStates)
+        load_next_sequence = rospy.ServiceProxy(sequence_service_name, \
+            LoadSequence)
 
         # to allow arduino to get parameters before services are called
         # (so that debug flag can be in effect during services)
@@ -35,8 +38,9 @@ class ValveTester:
 
         pins_to_test = sorted(left_pins + right_pins)
         if len(pins_to_test) == 0:
-            raise ValueError('need some pins to test. specify olf/right_pins ' + \
-                'and olf/left_pins as ROS parameters that are lists of integers.')
+            raise ValueError('need some pins to test. specify olf/right_pins '+\
+                'and olf/left_pins as ROS parameters that are lists of ' + \
+                'integers.')
 
         # all the valves should have their control pins LOW by default (0v)
         default_states = [DefaultState(p, False) for p in \
@@ -58,20 +62,24 @@ class ValveTester:
                 h = Header()
                 now = rospy.Time.now()
                 h.stamp = now
-                # TODO why these? why not set via parameters? (diff file though? in package?)
-                start = now + rospy.Duration.from_sec(1.5)
-                end = now + rospy.Duration.from_sec(6.5)
+                # TODO why these? why not set via parameters? (diff file though?
+                # in package?)
+                start = now + rospy.Duration.from_sec(0.75)
+                end = now + rospy.Duration.from_sec(2.0)
                 high = Transition(t=start, s=State(ms_on=1, ms_off=0))
                 seq = Sequence(start=start, end=end, pins=[p], seq=[high])
-                this_pin_high = LoadSequenceRequest(header=h, seq=seq, pins_to_signal=[])
+                this_pin_high = LoadSequenceRequest(header=h, seq=seq, \
+                    pins_to_signal=[])
 
                 print('pin ' + str(p))
                 try:
                     resp = load_next_sequence(this_pin_high)
                 except rospy.ServiceException as exc:
-                    rospy.logerr("Service load_next_sequence failed: " + str(exc))
+                    rospy.logerr("Service load_next_sequence failed: " + \
+                        str(exc))
 
-                # TODO does it always sleep the period, or try to maintain period across all calls?
+                # TODO does it always sleep the period, or try to maintain
+                # period across all calls?
                 rate.sleep()
 
 
